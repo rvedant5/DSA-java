ca56b611a30288985e9d57111f6d518a1066da13
vedant <43410160+rvedant5@users.noreply.github.com>
2023-09-12 13:56:21 +0530

    Create Printnums.java

---
+++ 3. Loops/User Input Loops/Printnums.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner read = new Scanner(System.in);
+		int num = read.nextInt();
+		int a = 0;
+		// Update your code below this line
+		while(a < num){
+		    System.out.println(a);
+		    a++;
+		}
+
+	}
+}
